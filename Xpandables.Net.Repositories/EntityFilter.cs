caa5443e0ebd04f5c144f6467da7bbfa9fb6ca27
Francescolis <43299884+Francescolis@users.noreply.github.com>
2023-11-26 18:36:49 +0100

    add repository and states libraries

---
+++ Xpandables.Net.Repositories/EntityFilter.cs
@@ -0,0 +1,90 @@
+﻿
+/************************************************************************************************************
+ * Copyright (C) 2023 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+************************************************************************************************************/
+using System.Linq.Expressions;
+
+namespace Xpandables.Net.Repositories;
+
+/// <summary>
+/// Specifies base criteria with projection for entities.
+/// </summary>
+/// <typeparam name="TEntity">The type of the target entity.</typeparam>
+public record EntityFilter<TEntity> : EntityFilter<TEntity, TEntity>, IEntityFilter<TEntity>
+    where TEntity : class, IEntity
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="EntityFilter{TEntity}"/> to filter entities 
+    /// of <typeparamref name="TEntity"/> type and return result of <typeparamref name="TEntity"/> type.
+    /// </summary>
+    public EntityFilter() : base() { }
+
+    /// <summary>
+    /// Specifies the projection filter to be applied on specific type entities.
+    /// The value is : x => x;
+    /// </summary>
+    public sealed override Expression<Func<TEntity, TEntity>> Selector { get; init; } = x => x;
+}
+
+/// <summary>
+/// Specifies criteria with projection for entities to a specific result.
+/// </summary>
+/// <typeparam name="TEntity">The type of the target entity.</typeparam>
+/// <typeparam name="TResult">The type of result.</typeparam>
+/// <remarks>You must at least provides a value for the "Selector".</remarks>
+public record EntityFilter<TEntity, TResult> : IEntityFilter<TEntity, TResult>
+    where TEntity : class, IEntity
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="EntityFilter{TEntity, TResult}"/> to filter entities 
+    /// of <typeparamref name="TEntity"/> type and return result of <typeparamref name="TResult"/> type.
+    /// </summary>
+    /// <remarks>You must define the <see cref="EntityFilter{TEntity, TResult}.Selector"/> property, 
+    /// otherwise an error will occur.</remarks>
+    public EntityFilter() { }
+
+    ///<inheritdoc/>
+    public virtual Expression<Func<TEntity, bool>>? Criteria { get; init; }
+
+    ///<inheritdoc/>
+    public virtual Expression<Func<TEntity, TResult>> Selector { get; init; } = default!;
+
+    ///<inheritdoc/>
+    public virtual Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? OrderBy { get; init; }
+
+    ///<inheritdoc/>
+    public virtual Pagination? Paging { get; init; }
+
+    /// <summary>
+    /// Applies the filters to the specified queryable.
+    /// </summary>
+    /// <param name="queryable">The queryable to act on.</param>
+    /// <returns>The queryable instance where the filters have been applied.</returns>
+    public IQueryable<TResult> GetQueryableFiltered(IQueryable<TEntity> queryable)
+    {
+        if (Criteria is not null)
+            queryable = queryable.Where(Criteria);
+
+        if (OrderBy is not null)
+            queryable = OrderBy(queryable);
+
+        if (Paging is not null)
+            queryable = queryable
+                .Skip(Paging.Value.Index * Paging.Value.Size).Take(Paging.Value.Size);
+
+        return queryable.Select(Selector);
+    }
+}
---
+++ Xpandables.Net.States/State.cs
@@ -0,0 +1,46 @@
+﻿/************************************************************************************************************
+ * Copyright (C) 2023 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+************************************************************************************************************/
+namespace Xpandables.Net.States;
+
+/// <summary>
+/// The base State class declares methods that all Concrete State should
+/// implement and also provides a back reference to the Context object,
+/// associated with the State. This back reference can be used by States to
+/// transition the Context to another State.
+/// </summary>
+/// <typeparam name="TStateContext">The type of the context.</typeparam>
+public abstract class State<TStateContext> : IState<TStateContext>
+    where TStateContext : class, IStateContext
+{
+    /// <summary>
+    /// Gets the state context.
+    /// </summary>
+    protected TStateContext Context { get; private set; } = default!;
+
+    ///<inheritdoc/>
+    public void EnterState(TStateContext context)
+    {
+        OnEnteringStateContext(context);
+        Context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    /// <summary>
+    /// When overridden in derived class, allows author to modify the state context at runtime before it get applied.
+    /// </summary>
+    /// <param name="context">The target state context to act with.</param>
+    protected virtual void OnEnteringStateContext(TStateContext context) { }
+}
