7daf62a949c725e3bc57aaa47e2bd6339c3729da
Francis-Black EWANE <fewane@efb-entreprises.com>
2024-10-06 11:38:59 +0200

    Add IUnitOfWork interface and RepositoryExtensions class
    Introduced the IUnitOfWork interface for managing unit of work
    operations, including saving changes asynchronously and retrieving
    repositories. Added the RepositoryExtensions static class to provide
    utility methods for repository operations, such as combining and
    composing expressions and filtering sequences. Included copyright
    and licensing information in both files.

---
+++ Xpandables.Net/Repositories/IUnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+namespace Xpandables.Net.Repositories;
+
+/// <summary>
+/// Represents a unit of work that encapsulates a set of operations to 
+/// be performed as a single transaction.
+/// </summary>
+public interface IUnitOfWork : IAsyncDisposable
+{
+    /// <summary>
+    /// Saves all changes made in this unit of work.
+    /// </summary>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>The number of state entries written to the database.</returns>
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Gets the repository of the specified type.
+    /// </summary>
+    /// <typeparam name="TRepository">The type of the repository.</typeparam>
+    /// <returns>The repository instance.</returns>
+    IRepository GetRepository<TRepository>()
+        where TRepository : class, IRepository;
+
+    /// <summary>
+    /// Gets the read-only repository of the specified type.
+    /// </summary>
+    /// <typeparam name="TRepository">The type of the repository.</typeparam>
+    /// <returns>The read-only repository instance.</returns>
+    IRepositoryRead GetRepositoryRead<TRepository>()
+        where TRepository : class, IRepositoryRead;
+
+    /// <summary>
+    /// Gets the write-only repository of the specified type.
+    /// </summary>
+    /// <typeparam name="TRepository">The type of the repository.</typeparam>
+    /// <returns>The write-only repository instance.</returns>
+    IRepositoryWrite GetRepositoryWrite<TRepository>()
+        where TRepository : class, IRepositoryWrite;
+}
---
+++ Xpandables.Net/Repositories/RepositoryExtensions.cs
@@ -0,0 +1,109 @@
+﻿
+/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+using System.Linq.Expressions;
+
+namespace Xpandables.Net.Repositories;
+
+/// <summary>
+/// Provides extension methods for repository operations.
+/// </summary>
+public static class RepositoryExtensions
+{
+    /// <summary>
+    /// Combines two expressions into a single expression.
+    /// </summary>
+    /// <typeparam name="TFirstParam">The type of the first parameter.</typeparam>
+    /// <typeparam name="TIntermediate">The type of the intermediate result.</typeparam>
+    /// <typeparam name="TResult">The type of the final result.</typeparam>
+    /// <param name="first">The first expression.</param>
+    /// <param name="second">The second expression.</param>
+    /// <returns>A combined expression.</returns>
+    public static Expression<Func<TFirstParam, TResult>> Combine
+        <TFirstParam, TIntermediate, TResult>(
+        this Expression<Func<TFirstParam, TIntermediate>> first,
+        Expression<Func<TFirstParam, TIntermediate, TResult>> second)
+    {
+        ParameterExpression param = Expression
+            .Parameter(typeof(TFirstParam), "param");
+
+        Expression newFirst = first.Body
+            .Replace(first.Parameters[0], param);
+
+        Expression newSecond = second.Body
+            .Replace(second.Parameters[0], param)
+            .Replace(second.Parameters[1], newFirst);
+
+        return Expression
+            .Lambda<Func<TFirstParam, TResult>>(newSecond, param);
+    }
+
+    /// <summary>
+    /// Composes two expressions into a single expression.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the source parameter.</typeparam>
+    /// <typeparam name="TCompose">The type of the intermediate result.</typeparam>
+    /// <typeparam name="TResult">The type of the final result.</typeparam>
+    /// <param name="source">The source expression.</param>
+    /// <param name="compose">The compose expression.</param>
+    /// <returns>A composed expression.</returns>
+    public static Expression<Func<TSource, TResult>> Compose
+        <TSource, TCompose, TResult>(
+        this Expression<Func<TSource, TCompose>> source,
+        Expression<Func<TCompose, TResult>> compose)
+        where TCompose : notnull
+    {
+        ParameterExpression param = Expression.Parameter(typeof(TSource), null);
+        InvocationExpression invoke = Expression.Invoke(source, param);
+        InvocationExpression result = Expression.Invoke(compose, invoke);
+
+        return Expression.Lambda<Func<TSource, TResult>>(result, param);
+    }
+
+    /// <summary>  
+    /// Filters a sequence of values based on a predicate applied to a property 
+    /// of the elements.  
+    /// </summary>  
+    /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+    /// <typeparam name="TParam">The type of the property to filter.</typeparam>
+    /// <param name="source">An <see cref="IQueryable{T}"/> to filter.</param>
+    /// <param name="propertyExpression">The property expression to filter.</param>
+    /// <param name="predicate">A function to test each element for a condition.</param>
+    /// <returns>An <see cref="IQueryable{T}"/> that contains elements from the 
+    /// input sequence that satisfy the condition.</returns>
+    public static IQueryable<TSource> Where<TSource, TParam>(
+       this IQueryable<TSource> source,
+       Expression<Func<TSource, TParam>> propertyExpression,
+       Expression<Func<TParam, bool>> predicate)
+       where TParam : notnull =>
+       source.Where(propertyExpression.Compose(predicate));
+
+    internal static Expression Replace(
+           this Expression expression,
+           Expression searchEx,
+           Expression replaceEx)
+           => new ReplaceVisitor(searchEx, replaceEx).Visit(expression);
+
+    internal class ReplaceVisitor(Expression from, Expression to) :
+        ExpressionVisitor
+    {
+        private readonly Expression from = from, to = to;
+
+        public override Expression Visit(Expression? node)
+            => node == from ? to : base.Visit(node)!;
+    }
+}
