7d027efc896198d25ccf2c0934fee756ddb84df7
Francis-Black EWANE <fewane@efb-entreprises.com>
2024-10-06 10:26:45 +0200

    Add new interfaces for entities and filters
    Introduced new interfaces in the `Xpandables.Net.Repositories` namespace.
    
    ### IEntity.cs
    - Added `IEntity` interface for entities with unique identity.
    - Added generic `IEntity<TKey>` interface specifying identity type.
    - Included attributes to hide properties from editor and JSON.
    
    ### IEntityFilter.cs
    - Added `IEntityFilter` interface for entity filters with pagination.
    - Added methods for applying filters and fetching entities.
    - Introduced generic `IEntityFilter<TEntity, TResult>` for selection, predicates, and ordering.
    - Implemented `Apply` method for filtering and paginating queryables.
    - Added `IEntityFilter<TEntity>` for cases where result type matches entity type.

---
+++ Xpandables.Net/Repositories/IEntity.cs
@@ -0,0 +1,47 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace Xpandables.Net.Repositories;
+
+/// <summary>
+/// Represents an entity with a unique identity.
+/// </summary>
+public interface IEntity
+{
+    /// <summary>
+    /// Gets the object unique identity.
+    /// </summary>
+    object Id { get; }
+}
+
+/// <summary>
+/// Represents an entity with a unique identity of type <typeparamref name="TKey"/>.
+/// </summary>
+/// <typeparam name="TKey">The type of the unique identity.</typeparam>
+public interface IEntity<out TKey> : IEntity
+    where TKey : notnull, IComparable
+{
+    /// <summary>
+    /// Gets the object unique identity.
+    /// </summary>
+    new TKey Id { get; }
+
+    [EditorBrowsable(EditorBrowsableState.Never), JsonIgnore]
+    object IEntity.Id => Id;
+}
---
+++ Xpandables.Net/Repositories/IEntityFilter.cs
@@ -0,0 +1,128 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace Xpandables.Net.Repositories;
+/// <summary>
+/// Represents a filter for entities with pagination support.
+/// </summary>
+public interface IEntityFilter
+{
+    /// <summary>
+    /// Gets or sets the index of the page.
+    /// </summary>
+    ushort PageIndex { get; set; }
+
+    /// <summary>
+    /// Gets or sets the size of the page.
+    /// </summary>
+    ushort PageSize { get; set; }
+
+    /// <summary>
+    /// Applies the filter to the given queryable.
+    /// </summary>
+    /// <param name="queryable">The queryable to apply the filter to.</param>
+    /// <returns>The filtered queryable.</returns>
+    IQueryable Apply(IQueryable queryable);
+
+    /// <summary>
+    /// Fetches a collection of entities from the given queryable.
+    /// </summary>
+    /// <param name="queryable">The queryable to fetch entities from.</param>
+    /// <returns>A collection of entities.</returns>
+    public IEnumerable<IEntity> Fetch(IQueryable queryable)
+        => Apply(queryable).OfType<IEntity>();
+
+    /// <summary>
+    /// Fetches a collection of entities asynchronously from the given queryable.
+    /// </summary>
+    /// <param name="queryable">The queryable to fetch entities from.</param>
+    /// <param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+    /// <returns>An asynchronous collection of entities.</returns>
+    public IAsyncEnumerable<IEntity> FetchAsync(
+        IQueryable queryable,
+        CancellationToken cancellationToken = default)
+        => Apply(queryable).OfType<IEntity>().ToAsyncEnumerable();
+}
+
+/// <summary>
+/// Represents a filter for entities with pagination support and specific 
+/// selection criteria.
+/// </summary>
+/// <typeparam name="TEntity">The type of the entity.</typeparam>
+/// <typeparam name="TResult">The type of the result.</typeparam>
+public interface IEntityFilter<TEntity, TResult> : IEntityFilter
+    where TEntity : class, IEntity
+{
+    /// <summary>
+    /// Gets the selector expression for the entity.
+    /// </summary>
+    Expression<Func<TEntity, TResult>> Selector { get; }
+
+    /// <summary>
+    /// Gets the predicate expression for filtering entities.
+    /// </summary>
+    Expression<Func<TEntity, bool>>? Predicate { get; }
+
+    /// <summary>
+    /// Gets the function for ordering the entities.
+    /// </summary>
+    Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? OrderBy { get; }
+
+    /// <summary>
+    /// Applies the filter to the given queryable.
+    /// </summary>
+    /// <param name="queryable">The queryable to apply the filter to.</param>
+    /// <returns>The filtered queryable.</returns>
+    public IQueryable<TResult> Apply(IQueryable<TEntity> queryable)
+    {
+        IQueryable<TEntity> query = queryable;
+
+        if (Predicate is not null)
+        {
+            query = query.Where(Predicate);
+        }
+
+        if (OrderBy is not null)
+        {
+            query = OrderBy(query);
+        }
+
+        if (PageIndex > 0 && PageSize > 0)
+        {
+            query = query
+                .Skip((PageIndex - 1) * PageSize)
+                .Take(PageSize);
+        }
+
+        return query.Select(Selector);
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    IQueryable IEntityFilter.Apply(IQueryable queryable)
+        => Apply(queryable.OfType<TEntity>());
+}
+
+/// <summary>
+/// Represents a filter for entities with pagination support and specific 
+/// selection criteria.
+/// </summary>
+/// <typeparam name="TEntity">The type of the entity.</typeparam>
+public interface IEntityFilter<TEntity> : IEntityFilter<TEntity, TEntity>
+    where TEntity : class, IEntity
+{ }
