61310c236193f28f8ce5dfd22f04e6b6199c2688
Francis-Black EWANE <fewane@efb-entreprises.com>
2024-10-13 00:18:26 +0200

    Add `TestPrimitive` struct and `PrimitiveUnitTest` class
    Introduce `TestPrimitive` record struct implementing `IPrimitive`
    interface with required `Value` property of type `string`. Define
    methods `Create`, `Default`, and implicit conversion operators
    for `TestPrimitive`.
    
    Add `PrimitiveUnitTest` class with nested `IPrimitiveTests` class
    to verify `TestPrimitive` behavior, including tests for `Create`,
    `Default`, implicit conversions, and `IPrimitive` value property.
    
    Introduce `IPrimitive` and `IPrimitive<TPrimitive, TValue>`
    interfaces in `IPrimitive.cs` with properties and methods for
    primitive value handling and conversions.

---
+++ Xpandables.Net.Test/UnitTests/PrimitiveUnitTest.cs
@@ -0,0 +1,90 @@
+﻿using FluentAssertions;
+
+using Xpandables.Net.Text;
+
+namespace Xpandables.Net.Test.UnitTests;
+public sealed class PrimitiveUnitTest
+{
+    public readonly record struct TestPrimitive : IPrimitive<TestPrimitive, string>
+    {
+        public required string Value { get; init; }
+
+        public static TestPrimitive Create(string value) =>
+            new() { Value = value };
+
+        public static TestPrimitive Default() =>
+            new() { Value = string.Empty };
+
+        public static implicit operator string(TestPrimitive primitive) =>
+            primitive.Value;
+
+        public static implicit operator TestPrimitive(string value) =>
+            new() { Value = value };
+    }
+
+    public class IPrimitiveTests
+    {
+        [Fact]
+        public void Create_ShouldReturnPrimitiveWithValue()
+        {
+            // Arrange
+            string value = "test";
+
+            // Act
+            var primitive = TestPrimitive.Create(value);
+
+            // Assert
+            primitive.Value.Should().Be(value);
+        }
+
+        [Fact]
+        public void Default_ShouldReturnPrimitiveWithDefaultValue()
+        {
+            // Act
+            var primitive = TestPrimitive.Default();
+
+            // Assert
+            primitive.Value.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ImplicitConversion_ToValue_ShouldReturnPrimitiveValue()
+        {
+            // Arrange
+            var primitive = TestPrimitive.Create("test");
+
+            // Act
+            string value = primitive;
+
+            // Assert
+            value.Should().Be("test");
+        }
+
+        [Fact]
+        public void ImplicitConversion_ToPrimitive_ShouldReturnPrimitive()
+        {
+            // Arrange
+            string value = "test";
+
+            // Act
+            TestPrimitive primitive = value;
+
+            // Assert
+            primitive.Value.Should().Be(value);
+        }
+
+        [Fact]
+        public void IPrimitive_Value_ShouldReturnObjectValue()
+        {
+            // Arrange
+            IPrimitive primitive = TestPrimitive.Create("test");
+
+            // Act
+            object value = primitive.Value;
+
+            // Assert
+            value.Should().Be("test");
+        }
+    }
+
+}
---
+++ Xpandables.Net/Text/IPrimitive.cs
@@ -0,0 +1,75 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+using System.ComponentModel;
+
+namespace Xpandables.Net.Text;
+
+/// <summary>
+/// Represents a primitive value.
+/// </summary>
+public interface IPrimitive
+{
+    /// <summary>
+    /// Gets the value of the primitive.
+    /// </summary>
+    object Value { get; }
+}
+
+/// <summary>
+/// Represents a primitive value of a specific type.
+/// </summary>
+/// <typeparam name="TPrimitive">The type of the primitive.</typeparam>
+/// <typeparam name="TValue">The type of the value.</typeparam>
+public interface IPrimitive<TPrimitive, TValue> : IPrimitive
+    where TPrimitive : struct, IPrimitive<TPrimitive, TValue>
+    where TValue : notnull
+{
+    /// <summary>
+    /// Gets the value of the primitive.
+    /// </summary>
+    new TValue Value { get; }
+
+    /// <summary>
+    /// Creates a new instance of the primitive with the specified value.
+    /// </summary>
+    /// <param name="value">The value to create the primitive with.</param>
+    /// <returns>A new instance of the primitive.</returns>
+    static abstract TPrimitive Create(TValue value);
+
+    /// <summary>
+    /// Gets the default instance of the primitive.
+    /// </summary>
+    /// <returns>The default instance of the primitive.</returns>
+    static abstract TPrimitive Default();
+
+    /// <summary>
+    /// Defines an implicit conversion of a primitive to its value type.
+    /// </summary>
+    /// <param name="primitive">The primitive to convert.</param>
+    /// <returns>The value of the primitive.</returns>
+    static abstract implicit operator TValue(TPrimitive primitive);
+
+    /// <summary>
+    /// Defines an implicit conversion of a value type to its primitive type.
+    /// </summary>
+    /// <param name="value">The value to convert.</param>
+    /// <returns>A new instance of the primitive.</returns>
+    static abstract implicit operator TPrimitive(TValue value);
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    object IPrimitive.Value => Value;
+}
