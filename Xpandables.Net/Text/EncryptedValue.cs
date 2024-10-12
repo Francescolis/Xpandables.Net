063682937144faa62350ea539897b7c1d19c6997
Francis-Black EWANE <fewane@efb-entreprises.com>
2024-10-12 23:57:36 +0200

    Add EncryptedValue, RefreshTokenValue, and TokenValue structs
    Added three new record structs in the Xpandables.Net.Text namespace:
    - EncryptedValue: Represents an encrypted value with key and salt.
    - RefreshTokenValue: Represents a refresh token with expiration date.
    - TokenValue: Represents a token with value, type, and expiration date.
    
    All structs implement IEquatable<T> and include copyright and license info.

---
+++ Xpandables.Net/Text/EncryptedValue.cs
@@ -0,0 +1,41 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Xpandables.Net.Text;
+
+/// <summary>
+/// Represents an encrypted value with its associated key and salt.
+/// </summary>
+[DebuggerDisplay("Key = {Key}, Value = {Value}, Salt = {Salt}")]
+[StructLayout(LayoutKind.Auto)]
+public readonly record struct EncryptedValue : IEquatable<EncryptedValue>
+{
+    /// <summary>
+    /// Gets the key used for encryption.
+    /// </summary>
+    public required string Key { get; init; }
+    /// <summary>
+    /// Gets the encrypted value.
+    /// </summary>
+    public required string Value { get; init; }
+    /// <summary>
+    /// Gets the salt used for encryption.
+    /// </summary>
+    public required string Salt { get; init; }
+}
---
+++ Xpandables.Net/Text/TokenValue.cs
@@ -0,0 +1,46 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Xpandables.Net.Text;
+
+/// <summary>
+/// Represents a token with a value, type, and expiration date.
+/// </summary>
+[DebuggerDisplay("Value = {Value}, Type = {Type}, Expiration = {Expiration}")]
+[StructLayout(LayoutKind.Auto)]
+public readonly record struct TokenValue : IEquatable<TokenValue>
+{
+    /// <summary>
+    /// Gets the value of the token.
+    /// </summary>
+    public required string Value { get; init; }
+    /// <summary>
+    /// Gets the type of the token.
+    /// </summary>
+    public required string Type { get; init; }
+    /// <summary>
+    /// Gets the expiration date of the token.
+    /// </summary>
+    public required DateTime Expiration { get; init; }
+
+    /// <summary>
+    /// Gets a value indicating whether the token is expired.
+    /// </summary>
+    public bool IsExpired => DateTime.UtcNow > Expiration;
+}
