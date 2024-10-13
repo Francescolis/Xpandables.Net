43a9e82140f362a33b793c25ce8521b182322a5d
Francis-Black EWANE <fewane@efb-entreprises.com>
2024-10-13 12:58:10 +0200

    Add IHttpClientRequest interfaces for HTTP client requests
    Introduce IHttpClientRequest.cs with new interfaces:
    - IHttpClientRequest for basic HTTP client requests.
    - IHttpClientRequest<out TResponse> for typed responses.
    - IHttpClientAsyncRequest<out TResponse> for async stream responses.
    Includes 2024 copyright and Apache License 2.0.

---
+++ Xpandables.Net/Http/IHttpClientRequest.cs
@@ -0,0 +1,37 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+namespace Xpandables.Net.Http;
+
+/// <summary>
+/// Represents a request to be sent by an HTTP client.
+/// </summary>
+public interface IHttpClientRequest { }
+
+/// <summary>
+/// Represents a request to be sent by an HTTP client with a response 
+/// of type <typeparamref name="TResponse"/>.
+/// </summary>
+/// <typeparam name="TResponse">The type of the response.</typeparam>
+public interface IHttpClientRequest<out TResponse> : IHttpClientRequest { }
+
+/// <summary>
+/// Represents a request to be sent by an HTTP client with a stream response 
+/// of type <see cref="IAsyncEnumerable{T}"/> of <typeparamref name="TResponse"/> 
+/// type.
+/// </summary>
+/// <typeparam name="TResponse">The type of the response.</typeparam>
+public interface IHttpClientAsyncRequest<out TResponse> : IHttpClientRequest { }
