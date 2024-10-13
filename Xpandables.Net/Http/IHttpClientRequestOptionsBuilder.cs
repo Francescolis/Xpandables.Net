d4c3a206f40313dba9d8e66e7b08509d27ad8110
Francis-Black EWANE <fewane@efb-entreprises.com>
2024-10-13 15:10:51 +0200

    Add HTTP client configuration and request builder interfaces
    Introduced new functionality for configuring and managing HTTP client requests in a structured and extensible manner. Key changes include:
    
    - Added `HttpClientParameters.cs` for defining enums and constants related to HTTP client parameters.
    - Added `HttpClientRequestOptionsAttribute.cs` for configuring HTTP client request options via attributes.
    - Added `IHttpClientDispatcher.cs` as an empty interface for dispatching HTTP client requests.
    - Added `IHttpClientMime.cs` for MIME type retrieval based on file extensions.
    - Added `IHttpClientRequestOptionsBuilder.cs` for building `HttpClientRequestOptionsAttribute` using `IServiceProvider`.
    - Added `IHttpRequestBasicAuth.cs` for handling basic authentication in HTTP requests.
    - Added `IHttpRequestBuilder.cs` and `IHttpRequestCompleteBuilder.cs` for building HTTP content requests.
    - Added various interfaces in `Xpandables.Net.Http` for building specific types of HTTP requests, including byte array, cookies, form URL encoded, headers, multipart, PATCH, path strings, query strings, streams, and strings.
    
    These changes ensure a consistent and extensible approach to building and managing HTTP client requests.

---
+++ Xpandables.Net/Http/IHttpClientRequestOptionsBuilder.cs
@@ -0,0 +1,35 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+namespace Xpandables.Net.Http;
+
+/// <summary>
+/// Defines a builder interface for creating 
+/// <see cref="HttpClientRequestOptionsAttribute"/>.
+/// </summary>
+/// <remarks>This interface take priority over the 
+/// <see cref="HttpClientRequestOptionsAttribute"/>.</remarks>
+public interface IHttpClientRequestOptionsBuilder
+{
+    /// <summary>
+    /// Builds the <see cref="HttpClientRequestOptionsAttribute"/> using the 
+    /// provided <see cref="IServiceProvider"/>.
+    /// </summary>
+    /// <param name="serviceProvider">The service provider to use for 
+    /// building the options.</param>
+    /// <returns>The built <see cref="HttpClientRequestOptionsAttribute"/>.</returns>
+    HttpClientRequestOptionsAttribute Build(IServiceProvider serviceProvider);
+}
---
+++ Xpandables.Net/Http/IHttpRequestBuilder.cs
@@ -0,0 +1,27 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+namespace Xpandables.Net.Http;
+
+/// <summary>
+/// Provides with the base interface for all HTTP content request builders.
+/// </summary>
+public interface IHttpRequestBuilder { }
+
+/// <summary>
+/// Provides with the interface to complete the built.
+/// </summary>
+public interface IHttpRequestCompleteBuilder : IHttpRequestBuilder { }
