220b9707bdfb340e63769ad4cecb21930255156a
Francis-Black EWANE <fewane@efb-entreprises.com>
2024-10-16 18:10:53 +0200

    Refactor BuildAsync to use reflection and improve deserialization
    Introduce a private static readonly MethodInfo field `_doBuildAsync` to hold a reference to the `DoBuildAsync` method. Modify `BuildAsync` to use `_doBuildAsync` for dynamic invocation with the appropriate generic type argument, removing the `async` keyword from its signature. Add a new private static method `DoBuildAsync<TResult>` for deserialization and creation of `HttpClientResponse<TResult>`. Update deserialization logic to use the generic type parameter `TResult` directly. Replace `Activator.CreateInstance` with direct instantiation of `HttpClientResponse<TResult>`. Make minor adjustments for proper handling of deserialization results and response object creation.

--- Xpandables.Net/Http/ResponseBuilders/HttpClientResponseSuccessResultBuilder.cs
+++ Xpandables.Net/Http/ResponseBuilders/HttpClientResponseSuccessResultBuilder.cs
@@ -15,6 +15,7 @@
  *
 ********************************************************************************/
 using System.Net;
+using System.Reflection;
 using System.Text.Json;
 
 namespace Xpandables.Net.Http.ResponseBuilders;
@@ -23,6 +24,11 @@ namespace Xpandables.Net.Http.ResponseBuilders;
 /// </summary>
 public sealed class HttpClientResponseSuccessResultBuilder : IHttpClientResponseBuilder
 {
+    private static readonly MethodInfo _doBuildAsync =
+        typeof(HttpClientResponseSuccessResultBuilder)
+        .GetMethod(
+            nameof(DoBuildAsync),
+            BindingFlags.NonPublic | BindingFlags.Static)!;
     /// <inheritdoc/>
     public Type Type => typeof(HttpClientResponse<>);
 
@@ -33,7 +39,7 @@ public sealed class HttpClientResponseSuccessResultBuilder : IHttpClientResponse
             && (int)statusCode is >= 200 and <= 299;
 
     /// <inheritdoc/>
-    public async Task<TResponse> BuildAsync<TResponse>(
+    public Task<TResponse> BuildAsync<TResponse>(
         HttpClientResponseContext context,
         CancellationToken cancellationToken = default)
         where TResponse : HttpClientResponse
@@ -47,23 +53,34 @@ public sealed class HttpClientResponseSuccessResultBuilder : IHttpClientResponse
 
         Type resultType = typeof(TResponse).GetGenericArguments().First();
 
+        MethodInfo doBuildAsyncInvokable = _doBuildAsync
+            .MakeGenericMethod(resultType);
+
+        return (Task<TResponse>)doBuildAsyncInvokable
+            .Invoke(null, [context, cancellationToken])!;
+    }
+
+    private static async Task<HttpClientResponse<TResult>>
+       DoBuildAsync<TResult>(
+       HttpClientResponseContext context,
+       CancellationToken cancellationToken)
+    {
         using Stream stream = await context.Message.Content
              .ReadAsStreamAsync(cancellationToken)
              .ConfigureAwait(false);
 
-        object? result = stream is not null
-            ? await JsonSerializer.DeserializeAsync(
-                stream, resultType, context.SerializerOptions, cancellationToken)
+        TResult? result = stream is not null
+            ? await JsonSerializer.DeserializeAsync<TResult>(
+                stream, context.SerializerOptions, cancellationToken)
                 .ConfigureAwait(false)
-            : null;
+            : default;
 
-        return (TResponse)Activator.CreateInstance(
-            typeof(TResponse),
+        return new HttpClientResponse<TResult>(
             context.Message.StatusCode,
             context.Message.ToNameValueCollection(),
             result,
             null,
             context.Message.Version,
-            context.Message.ReasonPhrase)!;
+            context.Message.ReasonPhrase);
     }
 }
