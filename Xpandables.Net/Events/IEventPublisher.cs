9c843f80143d9743089174ab4a0b413e7b92241b
Francis-Black EWANE <fewane@efb-entreprises.com>
2024-10-17 23:40:59 +0200

    Add event handling interfaces and copyright headers
    Added copyright headers to `IEventHandler.cs`, `IEventPublisher.cs`,
    `IEventStore.cs`, and `IEventSubscriber.cs`. Introduced the
    `IEventHandler<TEvent>` interface for handling events asynchronously.
    Added the `IEventPublisher` interface for publishing single or multiple
    events asynchronously. Introduced the `IEventStore` interface for
    managing events with methods to append, fetch, and mark events as
    published. Added a `record struct` named `EventPublished` to represent
    published events. Introduced the `IEventSubscriber` interface for
    subscribing to events.

---
+++ Xpandables.Net/Events/IEventPublisher.cs
@@ -0,0 +1,53 @@
+﻿
+/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+using Xpandables.Net.Operations;
+
+namespace Xpandables.Net.Events;
+/// <summary>
+/// Defines a contract for publishing events.
+/// </summary>
+public interface IEventPublisher
+{
+    /// <summary>
+    /// Publishes the specified event asynchronously.
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the event.</typeparam>
+    /// <param name="event">The event to publish.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation 
+    /// requests.</param>
+    /// <returns>A task that represents the asynchronous operation. The task 
+    /// result contains the operation result.</returns>
+    Task<IOperationResult> PublishAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : notnull, IEvent;
+
+    /// <summary>
+    /// Publishes the specified events asynchronously.
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the events.</typeparam>
+    /// <param name="events">The events to publish.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation 
+    /// requests.</param>
+    /// <returns>A task that represents the asynchronous operation. The task 
+    /// result contains the operation result.</returns>
+    Task<IOperationResult> PublishAsync<TEvent>(
+        IEnumerable<TEvent> events,
+        CancellationToken cancellationToken = default)
+        where TEvent : notnull, IEvent;
+}
---
+++ Xpandables.Net/Events/IEventStore.cs
@@ -0,0 +1,81 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+namespace Xpandables.Net.Events;
+
+/// <summary>
+/// Represents a store for events that can be appended, fetched, 
+/// and marked as published.
+/// </summary>
+public interface IEventStore
+{
+    /// <summary>
+    /// Appends a collection of events to the store.
+    /// </summary>
+    /// <param name="events">The events to append.</param>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when appending events 
+    /// fails.</exception>
+    Task AppendAsync(
+        IEnumerable<IEvent> events,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Fetches events from the store based on the specified filter.
+    /// </summary>
+    /// <param name="filter">The filter to apply when fetching events.</param>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>An asynchronous enumerable of events.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when fetching events
+    /// fails.</exception>
+    IAsyncEnumerable<IEvent> FetchAsync(
+        IEventFilter filter,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Marks a collection of events as published in the store.
+    /// </summary>
+    /// <param name="events">The events to mark as published.</param>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when marking events
+    /// fails.</exception>
+    Task MarkAsPublishedAsync(
+        IEnumerable<EventPublished> events,
+        CancellationToken cancellationToken = default);
+}
+
+/// <summary>
+/// Represents an event that has been published.
+/// </summary>
+public readonly record struct EventPublished
+{
+    /// <summary>
+    /// Gets the unique identifier of the event.
+    /// </summary>
+    public required Guid EventId { get; init; }
+
+    /// <summary>
+    /// Gets the date and time when the event was published.
+    /// </summary>
+    public required DateTimeOffset PublishedOn { get; init; }
+
+    /// <summary>
+    /// Gets the error message if the event failed to publish.
+    /// </summary>
+    public required string? ErrorMessage { get; init; }
+}
