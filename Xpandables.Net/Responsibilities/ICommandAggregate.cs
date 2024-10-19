e0aeb9da900564e10797a796c74a45e1aa0c44f2
Francis-Black EWANE <fewane@efb-entreprises.com>
2024-10-19 23:08:24 +0200

    Add interfaces and namespaces for command/query patterns
    * Added copyright headers to multiple files, indicating the code is licensed under the Apache License, Version 2.0.
    * Introduced a new namespace `Xpandables.Net.Responsibilities` for organizing the interfaces.
    * Added `ICommand` interface as a marker for commands, intended to be used with `ICommandHandler<TCommand>`.
    * Added `ICommandAggregate<TAggregate, TKey>` and `ICommandAggregate<TAggregate>` interfaces to define the structure for command aggregates, supporting the Decider pattern.
    * Added `ICommandAggregateHandler<TCommand, TAggregate, TKey>` and `ICommandAggregateHandler<TCommand, TAggregate>` interfaces to define handlers for command aggregates, supporting the Decider pattern.
    * Added `IQuery<TResult>` interface as a marker for queries, intended to be used with `IQueryHandler<TQuery>`.
    * Added `IQueryAsync<TResult>` interface as a marker for asynchronous queries, intended to be used with `IQueryAsyncHandler<TQuery, TResult>`.

---
+++ Xpandables.Net/Responsibilities/ICommandAggregate.cs
@@ -0,0 +1,56 @@
+﻿
+/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+using Xpandables.Net.Events.Aggregates;
+using Xpandables.Net.Optionals;
+
+namespace Xpandables.Net.Responsibilities;
+
+/// <summary>
+/// Represents a command aggregate interface that defines the structure 
+/// for command aggregates.
+/// </summary>
+/// <remarks>It's used for implementing the Decider pattern. </remarks>
+/// <typeparam name="TAggregate">The type of the aggregate.</typeparam>
+/// <typeparam name="TKey">The type of the key identifier.</typeparam>
+public interface ICommandAggregate<TAggregate, TKey>
+    where TAggregate : class, IAggregate<TKey>, new()
+    where TKey : struct
+{
+    /// <summary>
+    /// Gets or sets the aggregate.
+    /// </summary>
+    Optional<TAggregate> Aggregate { get; set; }
+
+    /// <summary>
+    /// Gets the key identifier.
+    /// </summary>
+    TKey KeyId { get; }
+}
+
+
+/// <summary>
+/// Represents a command aggregate interface that defines the structure 
+/// for command aggregates using GUID identifier.
+/// </summary>
+/// <remarks>It's used for implementing the Decider pattern. </remarks>
+/// <typeparam name="TAggregate">The type of the aggregate.</typeparam>
+public interface ICommandAggregate<TAggregate> :
+    ICommandAggregate<TAggregate, Guid>
+    where TAggregate : class, IAggregate<Guid>, new()
+{
+}
---
+++ Xpandables.Net/Responsibilities/ICommandAggregateHandler.cs
@@ -0,0 +1,57 @@
+﻿
+/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+using Xpandables.Net.Events.Aggregates;
+using Xpandables.Net.Operations;
+
+namespace Xpandables.Net.Responsibilities;
+
+/// <summary>
+/// Defines a handler for command aggregates.
+/// </summary>
+/// <remarks>It's used for implementing the Decider pattern. </remarks>
+/// <typeparam name="TCommand">The type of the command.</typeparam>
+/// <typeparam name="TAggregate">The type of the aggregate.</typeparam>
+/// <typeparam name="TKey">The type of the key.</typeparam>
+public interface ICommandAggregateHandler<TCommand, TAggregate, TKey>
+    where TCommand : ICommandAggregate<TAggregate, TKey>
+    where TAggregate : class, IAggregate<TKey>, new()
+    where TKey : struct
+{
+    /// <summary>
+    /// Handles the specified command asynchronously.
+    /// </summary>
+    /// <param name="command">The command to handle.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The result of the operation.</returns>
+    Task<IOperationResult> HandleAsync(
+        TCommand command,
+        CancellationToken cancellationToken = default);
+}
+
+/// <summary>
+/// Defines a handler for command aggregates with a GUID key.
+/// </summary>
+/// <remarks>It's used for implementing the Decider pattern. </remarks>
+/// <typeparam name="TCommand">The type of the command.</typeparam>
+/// <typeparam name="TAggregate">The type of the aggregate.</typeparam>
+public interface ICommandAggregateHandler<TCommand, TAggregate> :
+    ICommandAggregateHandler<TCommand, TAggregate, Guid>
+    where TCommand : ICommandAggregate<TAggregate, Guid>
+    where TAggregate : class, IAggregate<Guid>, new()
+{
+}
