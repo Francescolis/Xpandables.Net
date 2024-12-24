bd28ad92ef2631feb3d020a4711a5dec944745ed
Francis-Black EWANE <fewane@efb-entreprises.com>
2024-12-24 17:48:33 +0100

    Update xunit package and enhance JSON serialization
    - Updated `xunit.runner.visualstudio` to version `3.0.0`.
    - Added `System.Text.Json.Serialization` namespace in `HttpClientOptions.cs`.
    - Introduced `Converters` property in `SerializerOptions` with `JsonStringEnumConverter` for improved enum handling.

--- Xpandables.Net/Http/HttpClientOptions.cs
+++ Xpandables.Net/Http/HttpClientOptions.cs
@@ -17,6 +17,7 @@
 using System.Net;
 using System.Reflection;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 using Xpandables.Net.Http.Interfaces;
 using Xpandables.Net.Http.RequestBuilders;
@@ -208,7 +209,11 @@ public sealed record HttpClientOptions
         {
             PropertyNameCaseInsensitive = true,
             PropertyNamingPolicy = null,
-            WriteIndented = true
+            WriteIndented = true,
+            Converters =
+            {
+                new JsonStringEnumConverter()
+            }
         };
     }
 
