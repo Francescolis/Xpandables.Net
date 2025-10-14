a27964042917a8a3ccb692b724b680d383d9a793
Francescolis <francisblack.ewane@gmail.com>
2025-10-14 23:34:31 +0200

    Refactor and enhance modular service registration
    Removed outdated license headers from existing files and added updated Apache License 2.0 headers to new files. Introduced `IEndpointRoute` and `IUseService` interfaces to enable modular service and endpoint registration in ASP.NET Core applications. Added `IEndpointRouteExtensions` and `IServiceExportCollectionExtensions` classes for dynamic discovery and configuration of routes and services.
    
    Improved code quality with null checks and attributes like `[RequiresUnreferencedCode]` to handle trimming scenarios. Added extensive XML documentation for better clarity and maintainability. Adjusted namespaces with `#pragma` directives to suppress folder structure warnings.

---
+++ Xpandables.Net.AspNetCore/FromModelBinder.cs
@@ -0,0 +1,373 @@
+﻿
+/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+using System.Globalization;
+using System.Reflection;
+using System.Text.Json;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Xpandables.Net;
+
+/// <summary>
+/// Abstract class that holds the <see cref="FromModelBinder"/> dictionary
+/// attributes.
+/// </summary>
+public abstract class FromModelBinder
+{
+    /// <summary>
+    /// Creates an instance of <see cref="FromModelBinder"/>.
+    /// </summary>
+    protected FromModelBinder() { }
+
+    /// <summary>
+    /// Contains a dictionary with key as attribute and the request path 
+    /// values matching the specified name.
+    /// </summary>
+    protected static IDictionary<Attribute, Func<HttpContext, string, object?>>
+        RequestAttributeModelReader
+        => new Dictionary<Attribute, Func<HttpContext, string, object?>>
+        {
+            { new FromHeaderAttribute(), (context, name)
+                => context.Request.Headers[name].FirstOrDefault() },
+            { new FromRouteAttribute(), (context, name)
+                => context.Request.RouteValues[name] },
+            { new FromQueryAttribute(), (context, name)
+                => context.Request.Query[CultureInfo.CurrentCulture.TextInfo.
+                    ToTitleCase( name)].FirstOrDefault() }
+        };
+}
+
+/// <summary>
+/// Model binder used to bind models from the specified attributes :
+/// <see cref="FromHeaderAttribute"/>, <see cref="FromRouteAttribute"/> and 
+/// <see cref="FromQueryAttribute"/>.
+/// <para>It tries to bind with a parameterless constructor, 
+/// if not found, uses serialization with <see cref="System.Text.Json"/>.</para>
+/// </summary>
+/// <typeparam name="TAttribute">the type of the attribute.</typeparam>
+public sealed class FromModelBinder<TAttribute> : FromModelBinder, IModelBinder
+     where TAttribute : Attribute, IBindingSourceMetadata, IModelNameProvider, new()
+{
+    ///<inheritdoc/>
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        ArgumentNullException.ThrowIfNull(bindingContext);
+
+        string? modelName = bindingContext.ModelMetadata.BinderModelName;
+        Type modelType = bindingContext.ModelMetadata.ModelType;
+
+        if (modelName is not null)
+        {
+            BindingWithModelName(bindingContext, modelName, modelType);
+        }
+        else
+        {
+            BindingWithModelType(bindingContext, modelType);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private static void BindingWithModelType(
+        ModelBindingContext bindingContext,
+        Type modelType)
+    {
+        List<PropertyInfo> modelProperties = [.. modelType.GetProperties().Where(p => p.GetSetMethod()?.IsPublic == true)];
+
+        object? model = default;
+
+        // try default constructor
+        if (modelProperties.Count == 0)
+        {
+            model = CreateDefaultInstance(bindingContext, modelType);
+            if (model is not null)
+            {
+                bindingContext.Result = ModelBindingResult.Success(model);
+            }
+
+            // no property to bind
+            return;
+        }
+
+        // create properties
+        Dictionary<string, object?> propertyValues =
+            CreateProperties(bindingContext, modelProperties);
+        if (!bindingContext.ModelState.IsValid)
+        {
+            return;
+        }
+
+        // try parameter constructor
+        model = CreateConstructorInstance(
+            bindingContext, modelType, propertyValues);
+        if (model is not null)
+        {
+            bindingContext.Result = ModelBindingResult.Success(model);
+            return;
+        }
+
+        // try parameterless constructor
+        model = CreateParameterlessInstance(
+            bindingContext, modelType, modelProperties, propertyValues);
+        if (model is not null)
+        {
+            bindingContext.Result = ModelBindingResult.Success(model);
+            return;
+        }
+
+        // try deserialization
+        model = CreateDeserializedInstance(
+            bindingContext, modelType, propertyValues);
+        if (model is not null)
+        {
+            bindingContext.Result = ModelBindingResult.Success(model);
+        }
+    }
+
+    private static void BindingWithModelName(
+        ModelBindingContext bindingContext,
+        string modelName,
+        Type modelType)
+    {
+        object? attributeValue = RequestAttributeModelReader[new TAttribute()](
+            bindingContext.HttpContext, modelName);
+
+        if (attributeValue is string value)
+        {
+            try
+            {
+                object? model = JsonSerializer
+                    .Deserialize(
+                    value, modelType, JsonSerializerOptions.Web);
+
+                bindingContext.Result = ModelBindingResult.Success(model);
+            }
+            catch (Exception exception)
+                when (exception is ArgumentNullException
+                        or JsonException
+                        or NotSupportedException)
+            {
+                _ = bindingContext.ModelState
+                    .TryAddModelException(bindingContext.ModelName, exception);
+            }
+        }
+        else
+        {
+            _ = bindingContext.ModelState
+                .TryAddModelError(bindingContext.ModelName, "Invalid value");
+        }
+    }
+
+    private static object? CreateDefaultInstance(
+        ModelBindingContext bindingContext,
+        Type modelType)
+    {
+        object? model = default;
+        if (modelType.GetConstructor(Type.EmptyTypes) is not null)
+        {
+            try
+            {
+                model = Activator.CreateInstance(modelType);
+            }
+            catch (Exception exception)
+                when (exception is ArgumentNullException
+                        or ArgumentException
+                        or NotSupportedException
+                        or TargetInvocationException
+                        or MethodAccessException
+                        or MemberAccessException
+                        or System.Runtime.InteropServices.InvalidComObjectException
+                        or MissingMethodException
+                        or System.Runtime.InteropServices.COMException
+                        or TypeLoadException)
+            {
+                _ = bindingContext.ModelState
+                    .TryAddModelException(bindingContext.ModelName, exception);
+            }
+        }
+
+        return model;
+    }
+
+    private static object? CreateDeserializedInstance(
+        ModelBindingContext bindingContext,
+        Type modelType,
+        Dictionary<string, object?> propertyValues)
+    {
+        try
+        {
+            string dictString = JsonSerializer
+                .Serialize(
+                propertyValues, JsonSerializerOptions.Web);
+            return JsonSerializer
+                .Deserialize(
+                dictString,
+                modelType,
+                JsonSerializerOptions.Web);
+        }
+        catch (Exception exception)
+            when (exception is ArgumentNullException
+                    or JsonException
+                    or NotSupportedException)
+        {
+            _ = bindingContext.ModelState
+                .TryAddModelException(bindingContext.ModelName, exception);
+
+            return default;
+        }
+    }
+
+    private static object? CreateConstructorInstance(
+        ModelBindingContext bindingContext,
+        Type modelType,
+        Dictionary<string, object?> propertyValues)
+    {
+        if (bindingContext.ModelState.IsValid)
+        {
+            try
+            {
+                return Activator.CreateInstance(
+                    modelType, [.. propertyValues.Values]);
+            }
+            catch (Exception exception)
+                when (exception is ArgumentNullException
+                        or ArgumentException
+                        or NotSupportedException
+                        or TargetInvocationException
+                        or MethodAccessException
+                        or MemberAccessException
+                        or System.Runtime.InteropServices.InvalidComObjectException
+                        or MissingMethodException
+                        or System.Runtime.InteropServices.COMException
+                        or TypeLoadException)
+            {
+                _ = bindingContext.ModelState
+                    .TryAddModelException(bindingContext.ModelName, exception);
+            }
+        }
+
+        return default;
+    }
+
+    private static object? CreateParameterlessInstance(
+        ModelBindingContext bindingContext,
+        Type modelType,
+        List<PropertyInfo> propertyInfos,
+        Dictionary<string, object?> propertyValues)
+    {
+        try
+        {
+            object? model = Activator.CreateInstance(modelType);
+            if (model is not null) // parameterless constructor
+            {
+                foreach (PropertyInfo property in propertyInfos)
+                {
+                    try
+                    {
+                        property.SetValue(model, propertyValues[property.Name]);
+                    }
+                    catch (Exception exception)
+                        when (exception is ArgumentNullException
+                                or ArgumentException
+                                or NotSupportedException
+                                or TargetInvocationException
+                                or MethodAccessException
+                                or MemberAccessException
+                                or System.Runtime.InteropServices.InvalidComObjectException
+                                or MissingMethodException
+                                or System.Runtime.InteropServices.COMException
+                                or TypeLoadException)
+                    {
+                        _ = bindingContext.ModelState
+                            .TryAddModelException(property.Name, exception);
+                    }
+                }
+            }
+            else
+            {
+                _ = bindingContext.ModelState
+                    .TryAddModelError(bindingContext.ModelName, "Invalid value " +
+                    ": creating instance null");
+            }
+
+            return model;
+        }
+        catch (Exception exception)
+            when (exception is ArgumentNullException
+                    or ArgumentException
+                    or NotSupportedException
+                    or TargetInvocationException
+                    or MethodAccessException
+                    or MemberAccessException
+                    or System.Runtime.InteropServices.InvalidComObjectException
+                    or MissingMethodException
+                    or System.Runtime.InteropServices.COMException
+                    or TypeLoadException)
+        {
+            _ = bindingContext.ModelState
+                .TryAddModelException(bindingContext.ModelName, exception);
+
+            return default;
+        }
+    }
+
+    private static Dictionary<string, object?> CreateProperties(
+        ModelBindingContext bindingContext,
+        List<PropertyInfo> propertyInfos)
+    {
+        Dictionary<string, object?> propertyValues = new(propertyInfos.Count);
+        PropertyInfo? currentPropertyInfo = default;
+        try
+        {
+            foreach (PropertyInfo property in propertyInfos)
+            {
+                currentPropertyInfo = property;
+                object? value = RequestAttributeModelReader[new TAttribute()](
+                    bindingContext.HttpContext, property.Name);
+                object? converted = default;
+
+                if (value is not null)
+                {
+                    converted = value.ChangeTypeNullable(
+                        property.PropertyType, CultureInfo.CurrentCulture);
+                }
+
+                propertyValues.Add(property.Name, converted);
+            }
+        }
+        catch (Exception exception)
+            when (exception is ArgumentNullException
+                    or ArgumentException
+                    or NotSupportedException
+                    or InvalidCastException
+                    or FormatException
+                    or OverflowException
+                    or InvalidCastException)
+        {
+            _ = currentPropertyInfo is not null
+                ? bindingContext.ModelState
+                    .TryAddModelException(currentPropertyInfo.Name, exception)
+                : bindingContext.ModelState
+                    .TryAddModelException(bindingContext.ModelName, exception);
+        }
+
+        return propertyValues;
+    }
+}
---
+++ Xpandables.Net.AspNetCore/IEndpointRouteExtensions.cs
@@ -0,0 +1,93 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2024 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Xpandables.Net.DependencyInjection;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+
+/// <summary>
+/// Provides extension methods for registering and configuring endpoint routes in an ASP.NET Core application.
+/// </summary>
+/// <remarks>These extension methods enable the discovery and registration of endpoint route implementations from
+/// specified assemblies, as well as the configuration of the application's request pipeline to use those routes. Use
+/// these methods to simplify the integration of modular endpoint routing patterns in your application.</remarks>
+public static class IEndpointRouteExtensions
+{
+    /// <summary>
+    /// Adds endpoint routes from the specified assemblies to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection to add the endpoint routes to.</param>
+    /// <param name="assemblies">The assemblies to scan for endpoint routes. 
+    /// If no assemblies are specified, the calling assembly is used.</param>
+    /// <returns>The updated service collection.</returns>
+    [RequiresUnreferencedCode("This method may be trimmed.")]
+    public static IServiceCollection AddXEndpointRoutes(
+        this IServiceCollection services,
+        params Assembly[] assemblies)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        assemblies = assemblies is { Length: > 0 } ? assemblies : [Assembly.GetCallingAssembly()];
+
+        List<Type> endpointTypes = [.. assemblies
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => type is
+            {
+                IsInterface: false,
+                IsGenericType: false,
+                IsClass: true,
+                IsSealed: true
+            }
+                && typeof(IEndpointRoute).IsAssignableFrom(type))];
+
+        foreach (var endpointType in endpointTypes)
+        {
+            services.Add(new ServiceDescriptor(
+                typeof(IEndpointRoute),
+                endpointType,
+                ServiceLifetime.Transient));
+        }
+
+        return services;
+    }
+
+    /// <summary>
+    /// Configures the application to use endpoint routes defined in the 
+    /// service collection.
+    /// </summary>
+    /// <param name="application">The <see cref="WebApplication"/> to configure.</param>
+    /// <returns>The configured <see cref="WebApplication"/>.</returns>
+    public static WebApplication UseXEndpointRoutes(this WebApplication application)
+    {
+        ArgumentNullException.ThrowIfNull(application);
+
+        IEnumerable<IEndpointRoute> endpointRoutes = application.Services
+            .GetServices<IEndpointRoute>();
+
+        foreach (var route in endpointRoutes)
+        {
+            route.AddRoutes(application);
+        }
+
+        return application;
+    }
+}
