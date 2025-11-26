bc97ac86ae0696d51edf7470042e1cf377d64ca0
Francescolis <francisblack.ewane@gmail.com>
2025-11-26 22:01:29 +0100

    Add event handlers for deposit/withdraw integration
    Added copyright headers to all modified files. Introduced `MoneyDepositEventHandler` and `MoneyWithdrawEventHandler` to handle deposit and withdraw events, respectively, by converting them into `MoneyDepositWithdrawIntegrationEvent`.
    
    Created the `MoneyDepositWithdrawIntegrationEvent` record to encapsulate shared event data. Added `MoneyDepositWithdrawIntegrationEventHandler` to update bank account balances in the database.

---
+++ Xpandables.Net.SampleApi/BankAccounts/Features/WithdrawBankAccount/MoneyWithdrawEventHandler.cs
@@ -0,0 +1,39 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2025 Kamersoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+********************************************************************************/
+using System.Events;
+using System.Events.Integration;
+
+using Xpandables.Net.SampleApi.BankAccounts.Accounts;
+using Xpandables.Net.SampleApi.CrossEvents;
+
+namespace Xpandables.Net.SampleApi.BankAccounts.Features.WithdrawBankAccount;
+
+public sealed class MoneyWithdrawEventHandler(IPendingIntegrationEventsBuffer pendingIntegration) : IEventHandler<MoneyWithdrawEvent>
+{
+    public Task HandleAsync(MoneyWithdrawEvent eventInstance, CancellationToken cancellationToken = default)
+    {
+        var integrationEvent = new MoneyDepositWithdrawIntegrationEvent
+        {
+            BankAccountId = eventInstance.StreamId,
+            Amount = -eventInstance.Amount,
+        };
+
+        pendingIntegration.Add(integrationEvent);
+
+        return Task.CompletedTask;
+    }
+}
